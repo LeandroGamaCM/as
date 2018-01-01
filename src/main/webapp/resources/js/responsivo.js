64339ea79b0438387fa00d94a9869c1bee28a661
luans <luans@LUAN-DESKTOP>
2018-01-01 19:07:33 -0300

    Responsividade pra mobile e inclusão de submenus no cabeçalho

---
+++ src/main/webapp/resources/js/responsivo.js
@@ -0,0 +1,25 @@
+var resp = function () {
+    var menuLateral = document.querySelector("#menu-lateral");
+    var menuLateral2 = document.querySelector("#menu-lateral2");
+    var menuLateral3 = document.querySelector("#menu-lateral3");
+
+    this.acionar = function () {
+        menuLateral.classList.toggle('toggleMenu');
+        menuLateral2.classList.toggle('toggleMenu2');
+        menuLateral3.classList.toggle('toggleMenu3');
+        return false();
+    };
+};
+
+var resp2 = function () {
+    var menuLateral = document.querySelector("#menu-lateral");
+    var menuLateral2 = document.querySelector("#btn-menu2");
+    
+    this.acionar = function () {
+        menuLateral.classList.toggle('toggleMenu1');
+        menuLateral2.classList.toggle('btn-menu2');
+        return false();
+    };
+};
+
+
---
+++ src/main/webapp/resources/js/submenus.js
@@ -0,0 +1,20 @@
+var submenu = function () {
+    var sub_menu = document.querySelector("#animado");
+    
+    this.acionar = function () {
+        sub_menu.classList.toggle('submenu');
+        return false();
+    };
+};
+
+var submenu2 = function () {
+    var sub_menu2 = document.querySelector("#respAnimado");
+    var sub_menu3 = document.querySelector("#respAnimado2");
+    
+    this.acionar = function () {
+        sub_menu2.classList.toggle('topbar-menu-active');
+        sub_menu2.classList.toggle('fadeInDown');
+        sub_menu3.classList.toggle('active-topmenuitem');
+        return false();
+    };
+};

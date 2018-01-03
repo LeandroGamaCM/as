4f64621618117dbd6dad0c5f34c832ae9c522a5a
luans <luans@LUAN-DESKTOP>
2018-01-02 23:53:27 -0300

    Novas telas para login e cadastro

---
+++ src/main/webapp/resources/js/focus.js
@@ -0,0 +1,14 @@
+//var input = document.getElementById('focusable5');
+//            var input2 = document.getElementById('focusable6');
+//
+//            input.onblur = verifyInput(input);
+//            input.onblur = verifyInput(input2);
+//
+//            function verifyInput(input) {
+//
+//                if (input.value !== null) {
+//                    input.classList.add('ui-state-filled');
+//                }else{
+//                    input.classList.remove('ui-state-filled');
+//                }
+//            }

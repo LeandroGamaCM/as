7c739f998040c59e13f2c38a7ce2a8f776b6bcf3
luans <luans@LUAN-DESKTOP>
2018-02-15 16:10:14 -0300

    Commit com função enviar email

--- src/main/java/br/edu/ifba/paae/emailService/EmailUtils.java
+++ src/main/java/br/edu/ifba/paae/emailService/EmailUtils.java
@@ -1,36 +1,56 @@
 package br.edu.ifba.paae.emailService;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
 
-public class EmailUtils {
- 
- private static final String HOSTNAME = "smtp.gmail.com";
- private static final String USERNAME = "suportepaae@gmail.com";
- private static final String PASSWORD = "sistemastrong71";
- private static final String EMAILORIGEM = "suportepaae@gmail.com";
- 
- public static Email conectaEmail() throws EmailException {
- Email email = new SimpleEmail();
- email.setHostName(HOSTNAME);
- email.setSmtpPort(587);
- email.setAuthenticator(new DefaultAuthenticator(USERNAME, PASSWORD));
- email.setTLS(true);
- email.setFrom(EMAILORIGEM);
- return email;
- }
- 
- public static void enviaEmail(Mensagem mensagem) throws EmailException {
- Email email = new SimpleEmail();
- email = conectaEmail();
- email.setSubject(mensagem.getTitulo());
- email.setMsg(mensagem.getMensagem());
- email.addTo(mensagem.getDestino());
- String resposta = email.send();
- FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Um e-mail para recuperar a senha foi enviado com sucesso para: " + mensagem.getDestino(), "Informação"));
- }
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class EmailUtils{
+    private static final String SERVIDOR_SMTP = "smtp.gmail.com";
+    private static final String PORTA_SERVIDOR_SMTP = "465";
+    private static final String EMAIL = "suportepaae@gmail.com";
+    private static final String SENHA = "sistemastrong71";
+    
+    public static void main(String[] args) throws AddressException, MessagingException{
+        Autenticacao autenticacao = new Autenticacao(EMAIL, SENHA);
+        Session session = Session.getDefaultInstance(getPropriedades(), autenticacao);
+        session.setDebug(true);
+        MimeMessage email = new MimeMessage(session);        
+        
+        //Aqui que substitui o meu email por uma variável que recebe o email do aluno
+        email.setRecipient(Message.RecipientType.TO, new InternetAddress("luansilva71@hotmail.com"));
+        email.setFrom(new InternetAddress(EMAIL));
+        email.setSubject("Teste de Email");       
+        
+        //Aqui que substitui o corpo da mensagem pelas credenciais do aluno (CPF e nova senha do aluno) 
+        email.setContent("Corpo da Mensagem", "text/plain");
+        email.setSentDate(new Date());
+        Transport envio = session.getTransport("smtp");
+        envio.connect(SERVIDOR_SMTP, EMAIL, SENHA);
+        email.saveChanges();
+        envio.sendMessage(email, email.getAllRecipients());
+        envio.close();
+        System.out.println("Enviado com sucesso!");
+    }
+    public static Properties getPropriedades(){
+        Properties config = new Properties();
+        config.setProperty("mail.transport.protocol", "smtp");
+        config.setProperty("mail.smtp.starttls.enable", "true");
+        config.setProperty("mail.smtp.host", SERVIDOR_SMTP);
+        config.setProperty("mail.smtp.auth", "true");
+        config.setProperty("mail.smtp.user", EMAIL);
+        config.setProperty("mail.debug", "true");
+        config.setProperty("mail.smtp.port", PORTA_SERVIDOR_SMTP);
+        config.setProperty("mail.smtp.socketFactory.port", PORTA_SERVIDOR_SMTP);
+        config.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        config.setProperty("mail.smtp.socketFactory.fallback", "false");
+        return config;
+    }
 }
